27b903ff807f96e2ec2764d6af56b0e1e9f1c1bc
zlg9 <35779029+zlg9folira@users.noreply.github.com>
2022-06-15 17:21:31 -0400

    Update convert.m
    Added support for specific classes

--- convert.m
+++ convert.m
@@ -2,17 +2,21 @@ clear all;
 close all;
 fclose('all');
 
+
 %------------------directory and format setting------------------
-Dir_in = 'data/';
-Dir_out = 'json/';
-Dir_ann = 'annotation/';
+Dir_in = '/Users/foadhm/Downloads/relidarperceptiondataset/data/';
+Dir_out = '/Users/foadhm/Downloads/relidarperceptiondataset/json/';
+Dir_ann = '/Users/foadhm/Downloads/relidarperceptiondataset/annotation/';
 In_format = 'txt';
 im_format = 'png';
 train_portion = 70;test_portion=20;val_portion=10;
 res = 0.1;
 map_dim = [150 150];
+img_size = [map_dim(1)/res map_dim(2)/res];
 im_dim = [map_dim(1)/res map_dim(2)/res];
 ann_class_name = ["Car","Van","Truck","Pedestrian","Person_sitting","Cyclist","Tram","Misc","DontCare"];
+wanted_class_name = ["Car","Van","Truck"];
+wanted_class_id = [0,1,2];
 ann_class_id = [0,1,2,3,4,5,6,7,8];
 
 
@@ -74,12 +78,15 @@ fprintf('------------CONVERSION STARTED-------------\n')
 Files = dir(sprintf('%s*.%s',Dir_in,In_format));
 
 for k=1:length(Files)
+    wanted_class_name_count = zeros(1,length(wanted_class_name));
+    valid_image_list = [];
+    valid = 0;
     %---------initialize different parts of COCO json format---------
     %categories
     s.categories = struct([]);
-    for i=1:length(ann_class_id)
-        scat.id = ann_class_id(i);
-        scat.name = ann_class_name(i);
+    for i=1:length(wanted_class_id)
+        scat.id = wanted_class_id(i);
+        scat.name = wanted_class_name(i);
         s.categories = [s.categories,scat];
     end
     %annotations
@@ -103,15 +110,12 @@ for k=1:length(Files)
             valid = false;
         else
             count = count +1;
-            %-------add this frame to "images" of the struct----------
-            simg.id = str2num(tline_split);
-            simg.file_name = sprintf('%s.%s',tline_split,im_format);
-            s.images = [s.images,simg];
 
             %-------------read annotations for this frame-------------
             f_ann = fopen(sprintf('%s%s.%s',Dir_ann,tline_split,In_format), 'rt');
             tline_ann = fgetl(f_ann);
             count_line = 0;
+            valid_image = false;
             while ischar(tline_ann)
                 if strcmp(class(tline_ann),'char') == 1
                     count_line = count_line +1;
@@ -119,26 +123,86 @@ for k=1:length(Files)
                     %-------break the annotation into 15 parts--------
                     split = string(strsplit(tline_ann," ")); 
                     %-----------build a struct from labels------------
-                    sann.id = str2num(sprintf('%s%s',num2str(count_line),num2str(simg.id)));
-                    sann.image_id = simg.id;
-                    sann.category_id = ann_class_id(find(ann_class_name == split(1)));
-                    bbw = str2num(split(10))/res; bbh = str2num(split(11))/res;
-                    tlx = str2num(split(12))/res+(im_dim(1)/2)-(bbw/2); tly = im_dim(2) - ((str2num(split(14))-0.27)/res+(im_dim(2)/2)+(bbh/2)); %LiDAR-CAM frame 0.27m transform
-                    th = str2num(split(15));
-                    if th>=deg2rad(90) % rotate Kitti theta 90 degrees clockwise to match COCO (0 is not front of car but x-axis in image)
-                        th  = th - deg2rad(270);
-                    else
-                        th = th + deg2rad(90);
+                    this_ann = ann_class_id(find(ann_class_name == split(1)));
+                    if sum(split(1) == wanted_class_name) > 0
+
+                        bbw = str2num(split(10))/res; bbh = str2num(split(11))/res;
+                        tlx = str2num(split(12))/res+(im_dim(1)/2)-(bbw/2); tly = im_dim(2) - ((str2num(split(14))-0.27)/res+(im_dim(2)/2)+(bbh/2)); %LiDAR-CAM frame 0.27m transform
+                        th = str2num(split(15));
+                        th_orig = th;
+                        % rotate Kitti theta 90 degrees clockwise to match COCO
+                        if th>=deg2rad(90)
+                            th  = th - deg2rad(270);
+                        else
+                            th = th + deg2rad(90);
+                        end
+                        
+                        sann.category_id = ann_class_id(find(ann_class_name == split(1)));
+                        sann.id = str2num(sprintf('%s%s',num2str(count_line),num2str(str2num(tline_split))));
+                        sann.image_id = str2num(tline_split);
+                        sann.bbox = [tlx tly bbw bbh th]; %[tlx,tly,w,h,theta] in Kitti image frame
+                        
+                        % rotate the bbox to calculate segmentation field
+                        bb = sann.bbox;
+                        x = [bb(1) bb(1)+bb(3) bb(1)+bb(3) bb(1)];
+                        y = [bb(2) bb(2) bb(2)+bb(4) bb(2)+bb(4)];
+                        % create a matrix of these points, which will be useful in future calculations
+                        v = [x;y];
+                        % choose a point which will be the center of rotation
+                        x_center = bb(1)+bb(3)/2;
+                        y_center = bb(2)+bb(4)/2;
+                        % create a matrix which will be used later in calculations
+                        center = repmat([x_center; y_center], 1, length(x));
+                        % define a 60 degree counter-clockwise rotation matrix
+                        theta = bb(end);       % pi/3 radians = 60 degrees
+                        R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+                        % do the rotation...
+                        shift = v - center;     % shift points in the plane so that the center of rotation is at the origin
+                        so = R*shift;           % apply the rotation about the origin
+                        vo = so + center;   % shift again so the origin goes back to the desired center of rotation
+                        % pick out the vectors of rotated x- and y-data
+                        x_rotated = vo(1,:);
+                        y_rotated = vo(2,:);
+                        vr = [x_rotated;y_rotated];
+                        
+                        A = cell(1,1); A{1} = [x_rotated(1) y_rotated(1) x_rotated(2) y_rotated(2) x_rotated(3) y_rotated(3) x_rotated(4) y_rotated(4)];
+                        sann.segmentation = A;
+                        %sann.segmentation = [x_rotated(1) y_rotated(1) x_rotated(2) y_rotated(2) x_rotated(3) y_rotated(3) x_rotated(4) y_rotated(4)];
+                        sann.area = bbw * bbh; % [w*h] 
+                        sann.iscrowd = 0;
+                            
+                        % Make sure this ann is inside image
+                        if (tlx+bbw/2>img_size(1) || tlx-bbw/2<0 || tly+bbh/2>img_size(2) || tly-bbh/2<0) == 1
+                            % this ann is out of image frame
+                            %fprintf("error for image %s\n",tline_split);
+                        else
+                            %-----Count ann-----
+                            cc = find(ann_class_name == split(1));
+                            wanted_class_name_count(cc) = wanted_class_name_count(cc) + 1; % count class
+                            %-----add this to "annotations" of the struct-----
+                            s.annotations = [s.annotations,sann];
+                        end
+                        
+                        
+                        if ~valid_image && sum(size(s.annotations)) > 0
+                            valid_image = true;
+                        end
                     end
-                    sann.bbox = [tlx tly bbw bbh th]; %[tlx,tly,w,h,theta] in Kitti image frame
-                    sann.area = bbw * bbh; % [w*h] 
-                    sann.iscrowd = 0;
-                    %-----add this to "annotations" of the struct-----
-                    s.annotations = [s.annotations,sann];
                 end
                 tline_ann = fgetl(f_ann);
             end
             fclose(f_ann);
+            
+            %-------add this frame to "images" of the struct----------
+            if valid_image
+                simg.id = str2num(tline_split);
+                simg.file_name = sprintf('%s.%s',tline_split,im_format);
+                simg.height = img_size(2);
+                simg.width = img_size(1);
+                s.images = [s.images,simg];
+                valid = valid +1;
+            end
+
         end
         %fprintf('dataset: %s file: %s %d objects\n',name,tline_split,count_line)% disp(count_line);
         tline_split = fgetl(f_split);
@@ -152,8 +216,8 @@ for k=1:length(Files)
     fprintf(f_js, prettyjson(js));
     fclose(f_js);
 
-    fprintf('dataset %s: processed %d frames\n', name,count);
-
+    fprintf('dataset %s: processed %d frames (%d valid)\n', name,count,valid);
+    fprintf('\t%s:%d, %s:%d, %s:%d\n',wanted_class_name(1),wanted_class_name_count(1),wanted_class_name(2),wanted_class_name_count(2),wanted_class_name(3),wanted_class_name_count(3));
 end
 fprintf('------------CONVERSION COMPLETE------------\n')
 
